a1dfe37186dce2558468284461f58f0096e1bbd0
Amitbalter <amitbaltern@gmail.com>
2025-01-14 21:49:30 +0000

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function Home(){
       </ul>
     </nav>
     <a href="https://github.com/Amitbalter/chess" target="_blank">The source code can be found here</a>
+    <p>testing</p>
   </div>)
 }
 
