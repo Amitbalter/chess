cdae232a5a020e23a0853ed916ebca153d949523
Amitbalter <amitbaltern@gmail.com>
2025-02-17 00:20:37 +0000

    another attempt

--- client/src/Components/SocketContext.jsx
+++ client/src/Components/SocketContext.jsx
@@ -4,9 +4,7 @@ import { createContext } from "react";
 export const SocketContext = createContext();
 
 const serverUrl = process.env.NODE_ENV === "production" ? import.meta.env.VITE_SERVER_URL : "http://localhost:1234";
-const socket = io.connect(serverUrl, {
-    withCredentials: true,
-});
+const socket = io.connect(serverUrl);
 
 export default function SocketProvider({ children }) {
     return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
